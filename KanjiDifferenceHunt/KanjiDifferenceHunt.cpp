0493576e9a50711cb9bf3c36a3572632b7399945
広記 由井 <173224@ocsjoho.onmicrosoft.com>
2019-10-11 22:16:20 +0900

    Fisrt Commit

--- KanjiDifferenceHunt/KanjiDifferenceHunt.cpp
+++ KanjiDifferenceHunt/KanjiDifferenceHunt.cpp
@@ -1,8 +1,14 @@
 ﻿#include "stdafx.h"
+#include "Form1.h"
 
 using namespace System;
+using namespace KanjiDifferenceHunt;
 
+[STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew Form1());
     return 0;
 }
